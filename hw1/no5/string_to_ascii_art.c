015f03303374bfd2b11554451d08c20129bb546d
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-05 21:51:13 +0000

    fin

--- hw1/no5/string_to_ascii_art.c
+++ hw1/no5/string_to_ascii_art.c
@@ -158,208 +158,217 @@ int main(void) {
         {"#######     "}
     };
 
-    char t[5][12] = {
-        {"#########   "},
-        {"    #       "},
-        {"    #       "},
-        {"    #       "},
-        {"    #       "}
-    };
+  char t[5][12] = {
+    {"#########   "},
+    {"    #       "},
+    {"    #       "},
+    {"    #       "},
+    {"    #       "}
+  };
 
-    char u[5][12] = {
-        {"#       #   "},
-        {"#       #   "},
-        {"#       #   "},
-        {"#       #   "},
-        {" #######    "}
-    };
+  char u[5][12] = {
+    {"#       #   "},
+    {"#       #   "},
+    {"#       #   "},
+    {"#       #   "},
+    {" #######    "}
+  };
 
-    char v[5][12] = {
-        {"#       #   "},
-        {"#       #   "},
-        {" #     #    "},
-        {"  #   #     "},
-        {"    #       "}
-    };
+  char v[5][12] = {
+    {"#       #   "},
+    {"#       #   "},
+    {" #     #    "},
+    {"  #   #     "},
+    {"    #       "}
+  };
 
-    char w[5][12] = {
-        {"#       #   "},
-        {"#       #   "},
-        {"#   #   #   "},
-        {"# #  #  #   "},
-        {"#       #   "}
-    };
+  char w[5][12] = {
+    {"#       #   "},
+    {"#       #   "},
+    {"#   #   #   "},
+    {"# #  #  #   "},
+    {"#       #   "}
+  };
 
-    char x[5][12] = {
-        {"#       #   "},
-        {"  #   #     "},
-        {"    #       "},
-        {"  #   #     "},
-        {"#       #   "}
-    };
+  char x[5][12] = {
+    {"#       #   "},
+    {"  #   #     "},
+    {"    #       "},
+    {"  #   #     "},
+    {"#       #   "}
+  };
 
-    char y[5][12] = {
-        {"#       #   "},
-        {"  #   #     "},
-        {"    #       "},
-        {"    #       "},
-        {"    #       "}
-    };
+  char y[5][12] = {
+    {"#       #   "},
+    {"  #   #     "},
+    {"    #       "},
+    {"    #       "},
+    {"    #       "}
+  };
 
-    char z[5][12] = {
-        {"#########   "},
-        {"      #     "},
-        {"    #       "},
-        {"  #         "},
-        {"#########   "}
-    };
+  char z[5][12] = {
+    {"#########   "},
+    {"      #     "},
+    {"    #       "},
+    {"  #         "},
+    {"#########   "}
+  };
 
-    char space[5][12] = {
-      {"            "},
-      {"            "},
-      {"            "},
-      {"            "},
-      {"            "},
-    };
+  char space[5][12] = {
+    {"            "},
+    {"            "},
+    {"            "},
+    {"            "},
+    {"            "}
+  };
   
-    char input[15];
-    int inputLen = 0;
-    printf("What is your word?:");
-    fgets(input,15,stdin);
+  char input[30] = {""};
+  int inputLen = 0;
+  printf("What is your word?:");
+  fgets(input,30,stdin);
 
     //finds how many characters the user inputs
-    for(int issa = 0; issa < 30; issa++){
-        if(input[issa] == EOF){
-            break;
-        }
-        inputLen++;
-    }
-
-    for(int loc = 0; loc < inputLen; loc++){
-        input[loc] = toupper(input[loc]);
-    }
-
- for(int count = 0; count < inputLen; count++){
-        char in = input[count];
-        if(in == 'A'){
-            add(a, count);
-        }
-
-        if(in == 'B'){
-              add(b, count);
-        }
-
-        if(in == 'C'){
-            add(c, count);
-        }
-
-        if(in == 'D'){
-            add(d, count);
-        }
-
-        if(in == 'E'){
-            add(e, count);
-        }
-
-        if(in == 'F'){
-            add(f, count);
-        }
-
-        if(in == 'G'){
-             add(g, count);
-        }
-
-        if(in == 'H'){
-             add(h, count);
-        }
-
-        if(in == 'I'){
-            add(i, count);
-        }
-
-        if(in == 'J'){
-            add(j, count);
-        }
-
-        if(in == 'K'){
-            add(k, count);
-        }
-
-        if(in == 'L'){
-             add(l, count);
-        }
-
-        if(in == 'M'){
-          add(m, count);
-        }
-
-        if(in == 'N'){
-          add(n, count);
-        }
-
-        if(in == 'O'){
-           add(o, count);
-        }
-
-        if(in == 'P'){
-           add(p, count);
-        }
-
-        if(in == 'Q'){
-          add(q, count);
-        }
-
-        if(in == 'R'){
-          add(r, count);
-        }
-
-        if(in == 'S'){
-          add(s, count);
-        }
-         
-        if(in == 'T'){
-          add(t, count);
-        }
-
-        if(in == 'U'){
-          add(u, count);
-        }
-
-        if(in == 'V'){
-           add(v, count);
-        }
-
-        if(in == 'W'){
-          add(w, count);
-        }
-
-        if(in == 'X'){
-          add(x, count);
-        }
-
-        if(in == 'Y'){
-          add(y, count);
-        }
-
-        if(in == 'Z'){
-            add(z, count);
-        }
-
-        if(in == ' '){
-            add(space, count);
-        }
-    }
-
-        for(int second = 0; second < 5; second++){
-        for(int first = 0; first < inputLen; first++){
-            for(int third = 0; third < 12; third++){
-                printf("%c",arr[first][second][third]);
-            }
-        }
-        printf("\n");
+  for(int issa = 0; issa < 30; issa++){
+      if(input[issa] == 0){
+          inputLen = issa - 1;
+          break;
     }
 }
 
+char finput[inputLen];
+for(int count = 0; count < inputLen; count++){
+  finput[count] = input[count];
+}
+
+//makes all of the charact
+for(int loc = 0; loc < inputLen; loc++){
+  finput[loc] = toupper(finput[loc]);
+}
+
+//find what character the user input is and adds the character information to the 3d array
+for(int count = 0; count < inputLen; count++){
+  char in = finput[count];
+    if(in == 'A'){
+      add(a, count);
+    }
+
+    if(in == 'B'){
+      add(b, count);
+    }
+
+    if(in == 'C'){
+      add(c, count);
+    }
+
+    if(in == 'D'){
+      add(d, count);
+    }
+
+    if(in == 'E'){
+      add(e, count);
+    }
+
+    if(in == 'F'){
+      add(f, count);
+    }
+
+    if(in == 'G'){
+      add(g, count);
+    }
+
+    if(in == 'H'){
+      add(h, count);
+    }
+
+    if(in == 'I'){
+      add(i, count);
+    }
+
+    if(in == 'J'){
+      add(j, count);
+    }
+
+    if(in == 'K'){
+      add(k, count);
+    }
+
+    if(in == 'L'){
+      add(l, count);
+    }
+
+    if(in == 'M'){
+      add(m, count);
+    }
+
+    if(in == 'N'){
+      add(n, count);
+    }
+
+    if(in == 'O'){
+      add(o, count);
+    }
+
+    if(in == 'P'){
+      add(p, count);
+    }
+
+    if(in == 'Q'){
+      add(q, count);
+    }
+
+    if(in == 'R'){
+      add(r, count);
+    }
+
+    if(in == 'S'){
+      add(s, count);
+    }
+         
+    if(in == 'T'){
+      add(t, count);
+    }
+
+    if(in == 'U'){
+      add(u, count);
+    }
+
+    if(in == 'V'){
+      add(v, count);
+    }
+
+    if(in == 'W'){
+      add(w, count);
+    }
+
+    if(in == 'X'){
+      add(x, count);
+    }
+
+    if(in == 'Y'){
+      add(y, count);
+    }
+
+    if(in == 'Z'){
+      add(z, count);
+    }
+
+    if(in == ' '){
+      add(space, count);
+    }
+  }
+
+//prints the 3d array (print the each letter with hash)
+  for(int second = 0; second < 5; second++){
+    for(int first = 0; first < inputLen; first++){
+      for(int third = 0; third < 12; third++){
+        printf("%c",arr[first][second][third]);
+      }
+    }
+  printf("\n");
+  }
+}
+
+//creates a 3d array with the all of the characters the user inputs
 int add(char y[5][12], int z){
   int t = z;
   for(int i = 0; i < 5; i++){
